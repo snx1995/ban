36ba77a561e8da22a4a68b29380d41c86835856c
banyq <banyq@yonyou.com>
2019-09-25 14:17:32 +0800

    vue web

---
+++ src/web/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="index">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: 'It works!'
+        }
+    }    
+}
+</script>
+<style lang="less" scoped>
+
+</style>
