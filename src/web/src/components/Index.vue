7de9a2c23d414b548481ec836d257b9c6374bd55
banyq <banyq@yonyou.com>
2019-09-26 15:09:23 +0800

    update vue

--- src/web/src/components/Index.vue
+++ src/web/src/components/Index.vue
@@ -1,24 +1,71 @@
 <template>
     <div class="index">
-        <h1><i class="icon-spinner6"></i>{{title}}</h1>
-        <BButton :loading="true">Test</BButton>
+        <h1>{{title}}</h1>
+        <BButton type="primary" :loading="loading">
+            <template v-slot:icon>
+                <i class="icon-home"></i>
+            </template>
+            首页
+        </BButton>
+        <BButton :loading="loading">默认</BButton>
+        <BButton type="danger" :loading="loading">危险</BButton>
+        <BButton type="success" :loading="loading">成功</BButton>
+        <BButton type="info" :loading="loading">提示</BButton>
+        <BButton type="warning" :loading="loading">警告</BButton>
+        <br>
+        <br>
+        <BButton type="primary" @click="handleClick" size="sm" :loading="loading">
+            <template v-slot:icon>
+                <i class="icon-home"></i>
+            </template>
+            Test
+        </BButton>
+        <BButton type="primary" :loading="loading">Test</BButton>
+        <BButton type="primary" size="lg" :loading="loading">Test</BButton>
+        <br>
+        <br>
+        <BButton size="sm" :loading="loading">Test</BButton>
+        <BButton :loading="loading">Test</BButton>
+        <BButton size="lg" :loading="loading">Test</BButton>
+        <br>
+        <br>
+        <BButton type="danger" size="sm" :loading="loading">Test</BButton>
+        <BButton type="danger" :loading="loading">Test</BButton>
+        <BButton type="danger" size="lg" :loading="loading">Test</BButton>
+        <br>
+        <br>
+        <BButton type="success" size="sm" :loading="loading">Test</BButton>
+        <BButton type="success" :loading="loading">Test</BButton>
+        <BButton type="success" size="lg" :loading="loading">Test</BButton>
+        <br>
+        <br>
+        <BButton type="info" size="sm" :loading="loading">Test</BButton>
+        <BButton type="info" :loading="loading">Test</BButton>
+        <BButton type="info" size="lg" :loading="loading">Test</BButton>
+        <br>
+        <br>
+        <BButton type="warning" size="sm" :loading="loading">Test</BButton>
+        <BButton type="warning" :loading="loading">Test</BButton>
+        <BButton type="warning" size="lg" :loading="loading">Test</BButton>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            title: 'It works!'
+            title: 'It works!',
+            loading: false
         }
-    }    
+    },
+    methods: {
+        handleClick() {
+            this.loading = !this.loading;
+        }
+    }
 }
 </script>
 <style lang="less" scoped>
 .index {
     .borderBox();
-    i {
-        display: inline-block;
-        animation: rotate 1s linear infinite;
-    }
 }
 </style>
--- src/web/src/components/ban/button/Button.vue
+++ src/web/src/components/ban/button/Button.vue
@@ -1,12 +1,13 @@
 <template>
-    <button class="ban-btn" @click.stop="handleClick">
-        <i class="icon-spinner6 i-loading" v-if="loading"></i>
+    <button class="ban-btn" :class="[types[type], size]" @click.stop="handleClick">
+        <slot name="icon" class="btn-icon"></slot>
+        <i class="icon-spinner i-loading" v-if="loading"></i>
         <slot></slot>
     </button>
 </template>
 <script>
 export default {
-    name: 'ban-btn',
+    name: 'b-btn',
     props: {
         loading: {
             type: Boolean,
@@ -14,15 +15,26 @@ export default {
         },
         type: {
             type: String,
-            default: 'default',
+            default: 'default'
+        },
+        size: {
+            type: String,
+            default: 'md',
             validator(value) {
-                return '[default][primary][ghost]'.indexOf(`[${value}]`) > 0;
+                return value == 'sm' || value == 'md' || value == 'lg';
             }
         }
     },
     data() {
         return {
-
+            types: {
+                primary: 'primary',
+                success: 'success',
+                info: 'info',
+                warning: 'warning',
+                danger: 'danger',
+                default: 'default'
+            }
         }
     },
     methods: {
@@ -34,10 +46,66 @@ export default {
 </script>
 <style lang="less" scoped>
 button {
-    // .borderBox();
+    .borderBox();
+    display: inline-block;
+    border-radius: 6px;
+    cursor: pointer;
+    outline: none;
+    box-shadow: none;
+    border: 1px solid;
+    color: @fontColorLight;
+    &:active {
+        outline: none;
+        box-shadow: none;
+        filter: brightness(.9);
+    }
     .i-loading {
         display: inline-block;
         animation: rotate 1s linear infinite;
+    }
+    .btn-icon {
+        display: inline-block;
+    }
+    &.sm {
+        padding: 8px 9px;
+        font-size: 12px;
+    }
+    &.md {
+        padding: 11px 16px;
+        font-size: 14px;
+    }
+    &.lg {
+        padding: 13px 24px;
+        font-size: 18px;
+    }
+    &.primary {
+        background-color: @primary;
+        border-color: @primary;
+    }
+    &.success {
+        background-color: @success;
+        border-color: @success;
+    }
+    &.info {
+        background-color: @info;
+        border-color: @info;
+    }
+    &.warning {
+        background-color: @warning;
+        border-color: @warning;
+    }
+    &.danger {
+        background-color: @error;
+        border-color: @error;
+    }
+    &.default {
+        background-color: white;
+        border-color: @fontColorDark;
+        color: @fontColorDark;
+    }
+    &.ghost {
+        background-color: @ghost;
+        border-color: @ghost;
     }
 }
 </style>
