08258b587f91982c15ecee32cd0cb3a30b86c525
banyq <banyq@yonyou.com>
2020-04-08 15:32:46 +0800

    fix

--- src/main/java/art/banyq/persistent/dao/TestDao.java
+++
@@ -1,9 +0,0 @@
-package art.banyq.persistent.dao;
-
-import org.springframework.stereotype.Repository;
-import art.banyq.common.entity.vo.TestVO;
-
-@Repository
-public interface TestDao {
-    TestVO selectById(Integer id);
-}
